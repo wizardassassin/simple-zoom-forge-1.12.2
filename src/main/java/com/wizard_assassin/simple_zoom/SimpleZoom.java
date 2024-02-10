7e1a474aabf60d92e6741f2c05a1fe1d4ece9fba
Victor Chen <60045243+wizardassassin@users.noreply.github.com>
2024-02-10 15:04:02 -0800

    Renamed Files

---
+++ src/main/java/com/wizard_assassin/simple_zoom/SimpleZoom.java
@@ -0,0 +1,24 @@
+package com.wizard_assassin.simple_zoom;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = SimpleZoom.MOD_ID, clientSideOnly = true)
+public class SimpleZoom {
+    public static final String MOD_ID = "simple-zoom";
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+
+    @Mod.EventHandler
+    public void onPreInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+}
